1b93e6152126850f7adc286c1afadfc1aabbbd82
IamMikee <118041665+IamMikee@users.noreply.github.com>
2025-09-11 08:45:53 +0000

    mathgame

---
+++ mathgame.py
@@ -0,0 +1,47 @@
+import random
+import math
+
+highscore = 0
+score = 0
+diff = 1
+
+symbolarr = ["+", "-", "*", "/"]
+print(f"Welcome to the math game! Current highscore: {highscore}")
+
+def checkans(answer):
+    global score, diff, highscore
+    if answer:
+        score += 1
+        print(f"Correct! Current score: {score}")
+        # print(f"Current score: {score}")
+        diff = math.ceil(score/20)
+        run()
+    else:
+        print("Incorrect! Game over!")
+        print(f"Final Score : {score}")
+        if(score > highscore):
+            highscore = score
+        print(f"NEW HIGHSCORE! Highscore: {highscore}")
+        retry = input("Do you want to play again? (Y/N): ")
+        if retry == "y" or "Y" or "YES" or "yes" or "Yes":
+            print(f'Current highscore: {highscore}')
+            score = 0
+            run()
+        
+
+def run():
+    global diff
+    randomsymbol = random.randint(0,2)
+    number1, number2 = random.randint(10*diff-9,10*diff), random.randint(10*diff-9,10*diff)
+    print(f"What is {number1} {symbolarr[randomsymbol]} {number2}?")
+    ans = float(input(">>> "))
+    if randomsymbol == 0:
+        checkans(ans == float(number1+number2))
+    elif randomsymbol == 1:
+        checkans(ans == float(number1-number2))
+    elif randomsymbol == 2:
+        checkans(ans == float(number1*number2))
+    # elif randomsymbol == 3:
+    #     checkans(ans == float(number1/number2))
+    
+run()
