7ddb0297879a4e580eac548cbfe9022af17e7dff
IamMikee <gamingwithmike2020@gmail.com>
2025-11-27 00:13:30 +0800

    PyCharm Q2

--- q3.py
+++ q3.py
@@ -128,7 +128,31 @@ def evaluate_expression(expression: str) -> int:
     - No multiplication or division
     """
     # TODO: Implement expression evaluator using stack or recursion
+    def recursion(pos):
+        total = 0
+        num = 0
+        sign = 1
 
+        while pos < len(expression):
+            char = expression[pos]
+            if char.isdigit():
+                num = num*10 + int(char) #to update the num value as int (*10 to indicate tenths,hundreds,etc.)
+            elif char == "+":
+                total += num * sign
+                num = 0
+                sign = 1
+            elif char == "-":
+                total += num * sign
+                num = 0
+                sign = -1
+            elif char == "(":
+                num, pos = recursion(pos+1)
+            elif char == ")":
+                return total + sign*num, pos
+
+            pos += 1
+        return total + sign*num
+    return recursion(0)
 
 
 
@@ -182,17 +206,18 @@ if __name__ == '__main__':
     q1_trans = [[1, 4, 7, ],
                  [2, 5, 8, ],
                  [3, 6, 9, ]]
-    print(f"Q1 Answer is: matrix add: {q1_add == sol1.add(A, B)}\n"
-          f"              matrix subtract: {q1_sub == sol1.subtract(A, B)}\n"
-          f"              matrix multiply:{q1_mul == sol1.multiply(A, B)}\n"
-          f"              matrix transpose: {q1_trans == sol1.transpose(A)}\n")
+    # print(f"Q1 Answer is: matrix add: {q1_add == sol1.add(A, B)}\n"
+    #       f"              matrix subtract: {q1_sub == sol1.subtract(A, B)}\n"
+    #       f"              matrix multiply:{q1_mul == sol1.multiply(A, B)}\n"
+    #       f"              matrix transpose: {q1_trans == sol1.transpose(A)}\n")
 
 
 
 
-    # print("=== Q2 Testing ===")
-    # expressions = "7-(4-(1+2))"
-    # q2_ans = evaluate_expression(expressions)
+    print("=== Q2 Testing ===")
+    expressions = "(1-(2-(3))) + 4"
+    q2_ans = evaluate_expression(expressions)
+    # print(f"Q2 Answer is {q2_ans}")
     # print(f"Q2 Answer is {q2_ans == 6}")
     #
     #
