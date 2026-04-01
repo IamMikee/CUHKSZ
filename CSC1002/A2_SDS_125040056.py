74200334961f4c476b2aa0479343eb6188287599
IamMikee <gamingwithmike2020@gmail.com>
2026-04-01 15:17:21 +0800

    PyCharm

---
+++ CSC1002/A2_SDS_125040056.py
@@ -0,0 +1,197 @@
+import re
+
+content = ""
+cursor_pos = 0
+cursor_active = True
+help_text = """? - display this help info
+. - toggle row cursor on and off
+h - move cursor left
+l - move cursor right
+^ - move cursor to beginning of the line
+$ - move cursor to end of the line
+w - move cursor to beginning of next word
+b - move cursor to beginning of current or previous word
+e - move cursor to end of the word
+i - insert <text> before cursor
+a - append <text> after cursor
+I - insert <text> from beginning
+A - append <text> at the end
+x - delete character at cursor
+X - delete character before cursor
+dw - delete to start of next word
+de - delete to end of next word
+db - delete to start of current or previous word
+dc - delete whitespaces or entire word at cursor
+sw - swap word at cursor with next word
+sb - swap word at cursor with previous word
+v - view editor content
+q - quit program"""
+
+
+
+def moveto_next_word(text, position):
+    if position + 1 >= len(text) or not text: return position
+
+    while position < len(text) and text[position].isalnum():
+        position += 1
+
+    while position < len(text) and not text[position].isalnum():
+        position += 1
+
+    return position if position < len(text) else len(text)-1
+
+def moveto_previous_word(text, position):
+    if position == 0 or not text: return 0
+
+    prev_words_pos = [word.start() for word in re.finditer(r"\b\w", text[:position])]
+    return prev_words_pos[-1] if prev_words_pos else 0
+
+def moveto_end_of_word(text, position):
+    if position + 1 >= len(text) or not text: return position
+
+    current_word = re.search(r"\w+\b", text[position + 1:])
+    return position + current_word.end() if current_word else len(text)-1
+
+
+
+def insert_before_cursor(text, position, new_text):
+    return text[:position] + new_text + text[position:]
+
+def append_after_cursor(text, position, new_text):
+    if not text: return new_text, len(new_text) - 1
+    return text[:position + 1] + new_text + text[position + 1:], position + len(new_text)
+
+def insert_at_beginning(text, new_text):
+    return new_text + text, 0
+
+def append_at_end(text, new_text):
+    return text + new_text, len(text) + len(new_text) - 1
+
+
+
+def delete_on_cursor(text, position):
+    if not text: return "", 0
+    if len(text) - 1 == position: return text[:position], max(0, position - 1)
+    return text[:position] + text[position + 1:], position
+
+def delete_before_cursor(text, position):
+    if not text: return "", 0
+    if position == 0: return text, 0
+    return text[:position - 1] + text[position:], position - 1
+
+def delete_next_word(text, current_position, next_word_pos):
+    """
+        TODO:
+        CHECK AGAIN TO CHATGPT
+    """
+    if not text: return "", 0
+    if text[next_word_pos:]: return text[:current_position] + text[next_word_pos + 1:], current_position - 1
+    else: return text[:current_position], current_position - 1
+
+def delete_end_word(text, current_position, end_word_pos):
+    if not text: return "", 0
+    prev_text = text[:current_position]
+    if text[end_word_pos + 1:]: return prev_text + text[end_word_pos + 1:], current_position
+    else: return prev_text, len(prev_text) + 1
+
+
+
+def print_content(text, position, cursor_on):
+    if cursor_on and text:
+        print(f"{text[:position]}\033[42m{text[position]}\033[0m{text[position + 1:]}")
+    else:
+        print(text)
+
+
+
+while True:
+    prompt = input(">")
+
+    insert_cmds = ["i", "a", "I", "A"]
+
+    if not prompt:
+        continue
+
+    if len(prompt) == 1 and prompt[0] in insert_cmds: #invalid parameters for i,a,I,A
+        continue
+
+    match prompt[0]:
+        case "i":
+            content = insert_before_cursor(content, cursor_pos, prompt[1:])
+
+        case "a":
+            content, cursor_pos = append_after_cursor(content, cursor_pos, prompt[1:])
+
+        case "I":
+            content, cursor_pos = insert_at_beginning(content, prompt[1:])
+
+        case "A":
+            content, cursor_pos = append_at_end(content, prompt[1:])
+
+    match prompt:
+        case "?":
+            print(help_text)
+            continue
+
+        case ".":
+            cursor_active = not cursor_active
+
+        case "h":
+            if cursor_pos != 0:
+                cursor_pos -= 1
+
+        case "l":
+            if cursor_pos != len(content) - 1:
+                cursor_pos += 1
+
+        case "^":
+            cursor_pos = 0
+
+        case "$":
+            cursor_pos = max(0, len(content) - 1)
+
+        case "w":
+            cursor_pos = moveto_next_word(content, cursor_pos)
+
+        case "b":
+            cursor_pos = moveto_previous_word(content, cursor_pos)
+
+        case "e":
+            cursor_pos = moveto_end_of_word(content, cursor_pos)
+
+        case "x":
+            content, cursor_pos = delete_on_cursor(content, cursor_pos)
+
+        case "X":
+            content, cursor_pos = delete_before_cursor(content, cursor_pos)
+
+        case "dw":
+            content, cursor_pos = delete_next_word(content, cursor_pos, moveto_next_word(content, cursor_pos))
+
+        case "de":
+            content, cursor_pos = delete_end_word(content, cursor_pos, moveto_end_of_word(content, cursor_pos))
+
+        case "db":
+            pass
+
+        case "dc":
+            pass
+
+        case "sw":
+            pass
+
+        case "sb":
+            pass
+
+        case "v":
+            pass
+
+        case "q":
+            break
+
+        case _:
+            if prompt[0] not in insert_cmds:
+                continue
+
+
+    print_content(content, cursor_pos, cursor_active)
